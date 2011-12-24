76d730d8cf325b9a04677a4dd14f5379b1a6f892
Andrew Ritchie <ritchie@gmail.com>
2011-12-24 12:17:59 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sunshade::Application.config.secret_token = '5b739a5a0097506fb5a1c0f9e761c1b616874f36bf7cc33b91a737021164af9abf31077015fe5068419f4e8fa366c48db19284a3dadc8b6835c07150c0694cfa'
