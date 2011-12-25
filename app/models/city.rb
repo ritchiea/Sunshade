c9b85531503d69839fc801296156592b062f22c2
Andrew Ritchie <ritchie@gmail.com>
2011-12-24 19:59:45 -0500

    added rainy_today? method

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,17 @@
 class City < ActiveRecord::Base
   has_many :users
   
+  # TODO: write a validation that queries the weather API and checks for city
+  
+  # attr_accessor for rainy_today is necessary for weather API methods
   attr_accessor :rainy_today
     
+  def rainy_today?
+    if !!self.rainy_today
+      return true
+    else
+      return false
+    end
+  end
+  
 end
