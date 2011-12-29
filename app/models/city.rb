bc6e854cc97f5bac75180c1dac8f7d549ecc7f4e
Andrew Ritchie <ritchie@gmail.com>
2011-12-29 12:52:39 -0500

    generated citiescontroller

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,6 +2,7 @@ class City < ActiveRecord::Base
   has_many :users
   
   validates_presence_of :name
+
   
   # TODO: write a validation that queries the weather API and checks for city
   
@@ -14,6 +15,18 @@ class City < ActiveRecord::Base
     else
       return false
     end
+
+  
+  # write a method that sets a location to a variable
+  # checks .wet? for every hour during the workday on that variable
+  # if true then set rainy_today true
+  # if never true then set rainy_today false
+
+  def select_city(city_name)
+    @city = Barometer.new("#{city_name}")
   end
+
+  def check_forecast_for_city
+    select_city(name)
   
 end
