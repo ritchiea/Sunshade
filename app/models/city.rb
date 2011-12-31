33f75075419ecf73370f4b8df166f6a0c86d90ec
Andrew Ritchie <ritchie@gmail.com>
2011-12-31 13:27:47 -0500

    wrote check_all_forecasts class method in city model

--- app/models/city.rb
+++ app/models/city.rb
@@ -18,21 +18,24 @@ class City < ActiveRecord::Base
   end
   
   # write a method that sets a location to a variable
-  # checks .wet? for every hour during the workday on that variable
+  # checks .wet? 
   # if true then set rainy_today true
-  # if never true then set rainy_today false
+  # if not true then set rainy_today false
   
-  def check_all_forecasts
+  def self.check_all_forecasts
     cities = City.all
     cities.each do |z|
-      city = z.set_city_and_measure
-      report = city.check_for_rain
+      if z.check_for_rain
+        z.rainy_today = true
+      elsif !z.check_for_rain
+        z.rainy_today = false
+      end
     end
   end
   
   def check_for_rain
     city_forecast = self.set_city_and_measure
-    forecast_wet = city_forecast.wet?("9:00",25)
+    forecast_wet = city_forecast.wet?("13:00",25)
     return forecast_wet
   end
  
@@ -41,6 +44,5 @@ class City < ActiveRecord::Base
     full_forecast = city.measure
     return full_forecast
   end
-
 
 end
